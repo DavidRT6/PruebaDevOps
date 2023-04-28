f205df8b51fe20ac7b2d579504782ec3f2cf83da
David Roca <darota@floridauniversitaria.es>
2023-04-28 15:37:30 +0200

    iniciando

---
+++ test/CalculadoraTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+include './src/Calculadora.php';
+
+final class CalculadoraTest extends TestCase
+{
+	public function testSuma()
+	{
+		$calc = new Calculadora(3,4);
+		$this->assertEquals(7, $calc->suma());
+	}
+}
+?>
